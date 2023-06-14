dac8ac4f6f708037849a5dd6d8ac2d0726c724f0
redaphid <iam@aaronherres.com>
2023-06-14 11:34:24 -0700

    working minimal project

---
+++ index.test.ts
@@ -0,0 +1,7 @@
+import {beforeEach, describe, expect, it} from '@jest/globals';
+
+describe("when testing that jest works", () => {
+   it("should pass", () => {
+      expect(true).toBe(true);
+   });
+});
